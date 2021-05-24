49bf636154fbff90a7ab2b0b0239b1883454850b
Jeronimo Houlin <jeronimo.houlin@gmail.com>
2021-05-24 12:06:32 -0300

    1er intento de js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',function(){
-    //Acá vá el code posterior al Onload}
+    //Acá vá el code posterior al Onload
     const listOfInput = document.querySelectorAll('.headerblock')
-
+    //Tester
     for (let input of listOfInput) {
       input.addEventListener('mouseover', function(){
         input.style.color = "black";
@@ -11,10 +11,33 @@ window.addEventListener('load',function(){
       })
     }
 
+    //Search Results
 
+    fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27/top')
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(data){
+        //console.log(data)
+        let listaartistas = data;
+        console.log(listaartistas);
 
+        const searchbar = document.getElementById('searchbar');
 
-
+        searchbar.addEventListener('keyup', function(e){
+            const searchstring = e.target.value;
+            let filtered_search = function(searchstring){
+              return(
+                listaartistas.artist.name.includes(searchstring)||
+                listaartistas.artist.album.includes(searchstring)
+              );
+              console.log(filtered_search);
+            };
+        });
+      })
+      .catch(function(error){
+        console.log("el error fue:" + error);
+      })
 
 
 
