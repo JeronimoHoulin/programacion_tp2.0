42d53e73d57eb6a7ccac577f7554ca28e056a902
Jeronimo Houlin <jeronimo.houlin@gmail.com>
2021-05-29 13:09:22 -0300

    Seguimiento de JS // RDOS DE BUSQUEDA

--- js/index.js
+++ js/index.js
@@ -20,19 +20,27 @@ window.addEventListener('load',function(){
       .then(function(data){
         //console.log(data)
         let listaartistas = data;
-        console.log(listaartistas);
+        //console.log(listaartistas.data[1])
+        //console.log(listaartistas.data[0].title);
 
         const searchbar = document.getElementById('searchbar');
 
         searchbar.addEventListener('keyup', function(e){
             const searchstring = e.target.value;
-            let filtered_search = function(searchstring){
-              return(
-                listaartistas.artist.name.includes(searchstring)||
-                listaartistas.artist.album.includes(searchstring)
-              );
-              console.log(filtered_search);
+            //console.log(searchstring)
+          for(let i= 0 ; i<listaartistas.data.length; i++){
+            if(
+              listaartistas.data[i].album.title.includes(searchstring)||
+              listaartistas.data[i].artist.name.includes(searchstring)
+            ){
+                //console.log(listaartistas.data[i].album.title);
+                //console.log(listaartistas.data[i].artist.name);
+                let rdo_album = listaartistas.data[i].album.title;
+                let rdo_artista = listaartistas.data[i].artist.name;
+                document.getElementById("rdoslista").innerHTML = '<li>'+ rdo_album +'</li>';
+                document.getElementById("rdoslista").innerHTML = '<li>'+ rdo_artista +'</li>';
             };
+          };
         });
       })
       .catch(function(error){
